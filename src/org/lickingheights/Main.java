d0ebf015e64358fb3ada483159d9292f095605bf
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2019-01-23 21:04:56 -0500

    deleted first two lines

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("Hello");
-        System.out.println("World");
+        
 	System.out.println("RedBeard");
         System.out.println("is at his home computer");
     }
